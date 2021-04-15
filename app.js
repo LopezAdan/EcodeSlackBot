9b811f24e67592ffedc963da837d68f9c82c5afc
LopezAdan <adan1001917@gmail.com>
2021-04-14 21:52:22 -0700

    hello

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const slackEvents = createEventAdapter(slackSigningSecret);
 const slackClient = new WebClient(slackToken);
 
 
+
 slackEvents.on('app_mention',  (event)=> {
     console.log(`${event.channel} Got message from user ${event.user}: ${event.text}`);
     (async () => {
@@ -24,35 +25,49 @@ slackEvents.on('app_mention',  (event)=> {
     })();
 });
 
-slackEvents.on('app_mention',  (event)=> {
-    console.log(`${event.channel} Got message from user ${event.user}: ${event.text}`);
-    var momma = [ "yo momma", "yo moma", "yomomma", "yomoma", "YOMOMA", "YOMOMMA"];
-    var message = event.text.substr(event.text.indexOf(" ") + 1);
-
-    console.log(message);
-    console.log(momma.includes(message));
-    console.log(event.text)
-    if (momma.includes(message)) {
+/** Function for checking if a word in an array is 
+ * inside of a message. 
+ * message: the message sent by a user
+ * keywords: an array of words that are used to call an api
+ * **/
+function messageContainsKeyword(message, keywords) {
+    for (var i = 0; i < keywords.length; i++) {
+        if (message.includes(keywords[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+/** A function for generalizing simple api calls.
+ * Params:
+ * apiLink: The link to the api being used
+ * keywords: A list of keywords that will be used to call said function
+ * attributes: A list of strings, that are used to access more parts of data
+ *              ex. res.data.joke
+ *              => ['data', 'joke']
+ *   
+ */
+function callJokesAPI(apiLink, keywords, message, attributes) {
+    if (messageContainsKeyword(message, keywords)) {
         (async () => {
             try {
-                const baseUrl = 'https://api.yomomma.info/';
-                axios.get(`${baseUrl}`).then(res => 
-                    slackClient.chat.postMessage({channel: event.channel, text: res.data.joke}));
-            } catch(error) {
-                console.log(error.data)
-            }
-        })();
-    } else if (event.text.includes(" chucknorris")) {
-        (async () => {
-            try {
-                const baseUrl = 'http://api.icndb.com/jokes/random';
-                axios.get(`${baseUrl}`).then(res => 
-                    slackClient.chat.postMessage({channel: event.channel, text: res.data.value.joke}));
+                const baseUrl = apiLink;
+                const res = axios.get(`${baseUrl}`);
+                for (var i = 0; i < attributes.length; i++) {
+                    res = res[i];
+                }
+                slackClient.chat.postMessage({channel: event.channel, text: res});
             } catch(error) {
                 console.log(error.data)
             }
         })();
     }
+}
+
+slackEvents.on('app_mention',  (event)=> {
+    console.log(`${event.channel} Got message from user ${event.user}: ${event.text}`);
+    callJokesAPI('http://api.icndb.com/jokes/random', [' chucknorris'], event.text, ['data', 'value', 'joke']);
+    callJokesAPI("'https://api.yomomma.info/';", ['yo momma', 'yomomma', 'Yo momma'], event.text, ['data', 'joke']);
 });
 
 
