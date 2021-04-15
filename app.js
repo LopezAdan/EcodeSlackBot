80fe362f97297dd6132e93aca25e7ad5464b00d3
LopezAdan <adan1001917@gmail.com>
2021-04-14 20:55:45 -0700

    yo moma plus chuck

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { WebClient } = require('@slack/web-api');
 const { createEventAdapter } = require('@slack/events-api');
+const axios = require('axios').default;
 
 
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
@@ -11,19 +12,52 @@ const slackClient = new WebClient(slackToken);
 
 
 slackEvents.on('app_mention',  (event)=> {
-    console.log(`Got message from user ${event.user}: ${event.text}`);
+    console.log(`${event.channel} Got message from user ${event.user}: ${event.text}`);
     (async () => {
         try {
-            await slackClient.chat.postMessage({channel: event.channel, text: `Hello <@${event.user}>! :fadingparrot:`})
+            if (event.text.includes(" hello")) {
+                await slackClient.chat.postMessage({channel: event.channel, text: `Hello <@${event.user}>! :fadingparrot:`})
+            }
         } catch(error) {
             console.log(error.data)
         }
     })();
 });
 
+slackEvents.on('app_mention',  (event)=> {
+    console.log(`${event.channel} Got message from user ${event.user}: ${event.text}`);
+    var momma = [ "yo momma", "yo moma", "yomomma", "yomoma", "YOMOMA", "YOMOMMA"];
+    var message = event.text.substr(event.text.indexOf(" ") + 1);
+
+    console.log(message);
+    console.log(momma.includes(message));
+    console.log(event.text)
+    if (momma.includes(message)) {
+        (async () => {
+            try {
+                const baseUrl = 'https://api.yomomma.info/';
+                axios.get(`${baseUrl}`).then(res => 
+                    slackClient.chat.postMessage({channel: event.channel, text: res.data.joke}));
+            } catch(error) {
+                console.log(error.data)
+            }
+        })();
+    } else if (event.text.includes(" chucknorris")) {
+        (async () => {
+            try {
+                const baseUrl = 'http://api.icndb.com/jokes/random';
+                axios.get(`${baseUrl}`).then(res => 
+                    slackClient.chat.postMessage({channel: event.channel, text: res.data.value.joke}));
+            } catch(error) {
+                console.log(error.data)
+            }
+        })();
+    }
+});
+
+
 slackEvents.on("error", console.error);
 
 slackEvents.start(port).then(() => {
     console.log(`Server has started on port: ${port}`)
 });
-
